a0a00de2ea3b603488f4c9b2680b21d1335f0783
beavicario <bea.vg.9363@gmail.com>
2020-05-31 21:02:13 +0200

    MI TIENDA - AJAX

--- P3/Mitienda/client.js
+++ P3/Mitienda/client.js
@@ -1,11 +1,39 @@
 
 const cerveza = document.getElementById('cerveza')
 const resultado = document.getElementById('resultado')
+const lista = document.getElementById('lista')
+const precio = document.getElementById('precio')
+const vercarrito = document.getElementById('vercarrito')
+const email = document.getElementById('email')
+
+vercarrito.onclick = () => {
+  const m = new XMLHttpRequest();
+  m.open("GET", "http://localhost:8080/action.carrito?email=" + email.value, true)
+  m.onreadystatechange=function(){
+   //-- Petición enviada y recibida. Todo OK!
+   if (m.readyState==4 && m.status==200){
+
+     //-- La respuesta es un objeto JSON
+     let carrito = JSON.parse(m.responseText)
+
+     //-- Borrar el resultado anterior que hubiese en el párrafo
+     //-- de resultado
+     lista.innerHTML = "";
+
+     //--Recorrer los productos del objeto JSON
+     for (let i=0; i < carrito.articulos.length; i++) {
+       lista.innerHTML += carrito.articulos[i].replace("+", " ") + "<br>"
+     }
+     precio.innerHTML = "PRECIO TOTAL: " + carrito.total.toString() + "€"
+   }
+ }
+ m.send()
+}
 
 cerveza.onkeyup = () => {
   if (cerveza.value.length >= 3) {
     const m = new XMLHttpRequest();
-    m.open("GET", "http://localhost:8080/myquery.busqueda?cerveza=" + cerveza.value, true)
+    m.open("GET", "http://localhost:8080/action.busqueda?cerveza=" + cerveza.value, true)
     m.onreadystatechange=function(){
        //-- Petición enviada y recibida. Todo OK!
        if (m.readyState==4 && m.status==200){
--- P3/Mitienda/server-json.js
+++ P3/Mitienda/server-json.js
@@ -18,6 +18,14 @@ let productos = {
   "especiales": ["LAGARTA", "LA LOBA"],
 };
 
+const datos = {
+  "nombre": "",
+  "email" : "",
+  "localidad": "",
+  "articulos": [],
+  "total" : 0.0
+}
+
 //-- Funcion para atender a una Peticion
 //-- req: Mensaje de solicitud
 //-- res: Mensaje de respuesta
@@ -54,7 +62,6 @@ http.createServer((req, res) => {
       mime = "application/javascript";
       break;
     case "busqueda":
-      console.log(parametros.cerveza);
       for (let tipo in productos) {
         for (var i = 0; i < productos[tipo].length; i++) {
           if (productos[tipo][i].toLowerCase().indexOf(parametros.cerveza.toLowerCase())!= -1) {
@@ -66,26 +73,88 @@ http.createServer((req, res) => {
       res.write(JSON.stringify(cerveza))
       res.end()
       break;
+    case "carrito":
+      let email = parametros.email.replace("@", "%40")
+      let datacookie = {}
+      if (cookie) {
+        for (let c in cookie.split("; ")) {
+          if (cookie.split("; ")[c].split("=")[0] == email) {
+            datacookie = JSON.parse(cookie.split("; ")[c].split("=")[1])
+          }
+        }
+      }
+      res.setHeader("Content-Type","application/json")
+      res.write(JSON.stringify(datacookie))
+      res.end()
+      break;
+    case "compra":
+      if (req.method == "POST") {
+        req.on("data" , chunk => {
+          let data = chunk.toString()
+          let registrarse = false;
+          let email = data.split("&")[0].split("=")[1]
+          let cerveza = data.split("&")[1].split("=")[1]
+          let precio = data.split("&")[2].split("=")[1]
+          let datacookie = {}
+          let filename = ""
+          if (cookie) {
+            for (let c in cookie.split("; ")) {
+              if (cookie.split("; ")[c].split("=")[0] == email) {
+                registrarse = true;
+                datacookie = JSON.parse(cookie.split("; ")[c].split("=")[1])
+              }
+            }
+          }
+          if (!registrarse) {
+            filename = "./registro.html"
+          }else {
+            datacookie["articulos"].push(cerveza)
+            datacookie["total"] += parseFloat(precio)
+            res.setHeader("Set-Cookie", email + "=" + JSON.stringify(datacookie))
+            filename = "./index.html"
+          }
+          fs.readFile( filename, function(err, data) {
+
+            //-- Fichero no encontrado. Devolver mensaje de error
+            if (err) {
+              res.writeHead(404, {'Content-Type': 'text/html'});
+              return res.end("404 Not Found");
+            }
+
+            //-- Generar el mensaje de respuesta
+            res.writeHead(200, {'Content-Type': "text/html"});
+            res.write(data);
+            return res.end();
+          });
+          return
+        })
+      }
+      break;
     case "register":
       if (req.method == "POST") {
         req.on("data", chunk => {
           let data = chunk.toString()
           let nombre = data.split("&")[0].split("=")[1]
-          let email = data.split("&")[1].split("=")[1].replace(/[%40]/, "@")
+          let email = data.split("&")[1].split("=")[1]
           let localidad = data.split("&")[2].split("=")[1]
           let registrarse = false;
+          let datacookie = {}
+          let filename = "./index.html"
           if (cookie) {
             for (let c in cookie.split("; ")) {
-              console.log(cookie.split("; ")[c]);
               if (cookie.split("; ")[c].split("=")[0] == email) {
                 registrarse = true;
               }
             }
           }
           if (!registrarse) {
-            res.setHeader("Content-Type",email + "=" + nombre)
+            datacookie = datos
+            datacookie["nombre"] = nombre
+            datacookie["email"] = email
+            datacookie["localidad"] = localidad
+            res.setHeader("Set-Cookie",email + "=" + JSON.stringify(datacookie))
           }
-          fs.readFile("./index.html", function(err, data) {
+          fs.readFile( filename, function(err, data) {
 
             //-- Fichero no encontrado. Devolver mensaje de error
             if (err) {
@@ -97,7 +166,7 @@ http.createServer((req, res) => {
             res.writeHead(200, {'Content-Type': "text/html"});
             res.write(data);
             res.end();
-            return
+            return;
           });
           return
         })
@@ -107,7 +176,7 @@ http.createServer((req, res) => {
       mime = "text/html";
   }
 
-  if (extension != "busqueda" && extension != "register" ) {
+  if (q.pathname.indexOf("action") == -1 ) {
     fs.readFile(filename, function(err, data) {
 
       //-- Fichero no encontrado. Devolver mensaje de error
